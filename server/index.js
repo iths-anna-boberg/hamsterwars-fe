9e0f9005f45e2e399383dedc3b3b8788fca76431
iths-anna-boberg <amboberg@gmail.com>
2020-06-15 13:03:59 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,15 +5,9 @@ require('dotenv').config();
 
 const port = process.env.PORT || 2048;
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    const path = require ('path');
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
-// app.use(express.static(__dirname + '/../build')); 
+
+app.use(express.static(__dirname + '/../build')); 
 
 
 app.use(express.json());
