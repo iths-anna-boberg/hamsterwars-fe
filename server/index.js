d3cd5e90373a18a870181d9c5be8c7d166727d50
iths-anna-boberg <amboberg@gmail.com>
2020-06-15 13:00:05 +0200

    testar path för routing

--- server/index.js
+++ server/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+
 const port = process.env.PORT || 2048;
-app.use(express.static(__dirname + '/../build')); 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require ('path');
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+// app.use(express.static(__dirname + '/../build')); 
+
+
 app.use(express.json());
 
 
