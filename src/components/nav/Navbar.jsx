6eef4bbd174a5b2d2bc148026d9e8b69fc8b444c
iths-anna-boberg <amboberg@gmail.com>
2020-06-17 11:36:13 +0200

    close btn added to hamburger menu in mobile view

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -204,7 +204,10 @@ const Navbar = ()=>{
                 className="hamburger"
                 aria-hidden="true"
                 onClick={e => handleToggle(e)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+                    {isExpanded
+                    ?   <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="#2A4759"/></svg>
+                    :   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+                }
                 </button>
 
                 <ul className={`collapsed ${isExpanded ? 'is-expanded' : ''}`}>
